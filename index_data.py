92619c43e5a0965e90665e98528ee15bf9b5e349
RedZ_Nouggy <81537743+RedZNouggy@users.noreply.github.com>
2025-04-16 12:00:06 +0200

    Update index_data.py

--- index_data.py
+++ index_data.py
@@ -1,14 +1,19 @@
-from azure.search.documents.indexes import SearchIndexClient
-from azure.search.documents.indexes.models import (
-    SearchIndex, SimpleField, SearchableField, VectorSearch, HnswAlgorithmConfiguration, VectorField
-)
-from azure.search.documents import SearchClient
-from azure.core.credentials import AzureKeyCredential
-from config import *
-from utils import extract_text_from_pdf, chunk_text, get_embedding
+#!/usr/bin/env python3
 
 import os
 import uuid
+from azure.search.documents.indexes import SearchIndexClient
+from azure.search.documents.indexes.models import (
+    SearchIndex, SimpleField, SearchableField, VectorSearch, HnswAlgorithmConfiguration
+)
+from azure.search.documents import SearchClient
+from azure.core.credentials import AzureKeyCredential
+from config import (
+    AZURE_SEARCH_ENDPOINT,
+    AZURE_SEARCH_KEY,
+    AZURE_SEARCH_INDEX_NAME
+)
+from utils import extract_text_from_pdf, chunk_text, get_embedding
 
 def create_index():
     credential = AzureKeyCredential(AZURE_SEARCH_KEY)
@@ -18,7 +23,7 @@ def create_index():
         SimpleField(name="id", type="Edm.String", key=True),
         SearchableField(name="content", type="Edm.String"),
         SearchableField(name="source", type="Edm.String"),
-        VectorField(name="embedding", dimensions=1536, vector_search_configuration="default")
+        SearchableField(name="embedding", dimensions=1536, vector_search_configuration="default")
     ]
 
     vector_search = VectorSearch(
@@ -37,10 +42,11 @@ def create_index():
 
 def upload_chunks(pdf_path, source):
     credential = AzureKeyCredential(AZURE_SEARCH_KEY)
-    search_client = SearchClient(endpoint=AZURE_SEARCH_ENDPOINT,
-                                 index_name=AZURE_SEARCH_INDEX_NAME,
-                                 credential=credential)
-    
+    search_client = SearchClient(
+        endpoint=AZURE_SEARCH_ENDPOINT,
+        index_name=AZURE_SEARCH_INDEX_NAME,
+        credential=credential
+    )
     text = extract_text_from_pdf(pdf_path)
     chunks = chunk_text(text)
     docs = []
